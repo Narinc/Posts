c6731bea09b359c95cf7075eb5960a5f98321af4
volkannarinc <volkannarincc@gmail.com>
2026-09-15 15:38:47 +0300

    PostsTheme tema altyapısı kuruldu

--- app/src/main/java/com/narinc/posts/MainActivity.kt
+++ app/src/main/java/com/narinc/posts/MainActivity.kt
@@ -5,9 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.narinc.posts.ui.theme.PostsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme {
+            PostsTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     // TODO
                 }
